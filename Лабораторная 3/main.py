94a4533d95eccc6ef750fe7b692c09a5247aec83
anpian13 <113987820+anpian13@users.noreply.github.com>
2023-01-29 18:48:33 +0300

    Лабораторная 3

--- Лабораторная 3/main.py
+++ Лабораторная 3/main.py
@@ -1,8 +1,16 @@
 class Book:
     """ Базовый класс книги. """
     def __init__(self, name: str, author: str):
-        self.name = name
-        self.author = author
+        self._name = name
+        self._author = author
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def author(self):
+        return self._author
 
     def __str__(self):
         return f"Книга {self.name}. Автор {self.author}"
@@ -11,21 +19,47 @@ class Book:
         return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
 
 
-class PaperBook:
+class PaperBook(Book):
     def __init__(self, name: str, author: str, pages: int):
-        self.name = name
-        self.author = author
+        super().__init__(name, author)
+        self.pages = pages
+
+    @property
+    def pages(self) -> int:
+        return self._pages
+
+    @pages.setter
+    def pages(self, pages: int) -> None:
+        if not isinstance(pages, int):
+            raise TypeError("Количество страниц в книге должно быть типа int")
+        if pages <= 0:
+            raise ValueError("Количество страниц в книге должно быть больше 0")
         self.pages = pages
 
     def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+        return f"Книга {self.name}. Автор {self.author}. Количество страниц {self.pages}"
 
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages!r})"
 
-class AudioBook:
+class AudioBook(Book):
     def __init__(self, name: str, author: str, duration: float):
-        self.name = name
-        self.author = author
+        super().__init__(name, author)
         self.duration = duration
 
+    @property
+    def duration(self) -> int:
+        return self._duration
+
+    @duration.setter
+    def duration(self, duration: float) -> None:
+        if not isinstance(duration, float):
+            raise TypeError("Продолжительность книги должна быть типа float")
+        if duration <= 0:
+            raise ValueError("Продолжительность книги должна быть больше 0")
+
     def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+        return f"Книга {self.name}. Автор {self.author}. Продолжительность {self.duration}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration!r})"
